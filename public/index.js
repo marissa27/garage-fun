223bc87448e74086e08ad11778d95a45b3e63087
Marissa Reinke <reinke.marissa@gmail.com>
2017-06-01 16:22:54 -0600

    Do some styling so it doesn't look horrible

--- public/index.js
+++ public/index.js
@@ -4,12 +4,6 @@ $(document).ready(() => {
 
 let itemCount;
 
-appendCount = (number) => {
-  const $spanNumber = $('.item-number');
-  $spanNumber.html(
-    `<span class='item-number'>${number}</span>`);
-};
-
 getItems = () => {
   fetch('/api/v1/items', {
     method: 'GET'
@@ -26,16 +20,17 @@ getItems = () => {
   });
 };
 
-$('.submit').on('click', (e) => {
-  e.preventDefault();
-  let $name = $('.name').val();
-  let $reason = $('.reason').val();
-  let $cleanliness = $('.dropdown-form').val();
-  clearFields();
-  addItem($name, $reason, $cleanliness);
-});
+const grabItem = (id) => {
+  fetch(`/api/v1/items/${id}`)
+  .then(response => response.json())
+  .then(items => {
+    items.map((item) => {
+      appendOneItem(item.name, item.reason, item.cleanliness)
+    })
+  })
+}
 
-addItem = (name, reason, cleanliness) => {
+postItem = (name, reason, cleanliness) => {
   fetch('/api/v1/items', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
@@ -50,7 +45,7 @@ addItem = (name, reason, cleanliness) => {
   });
 };
 
-updateCount = () => {
+getCount = () => {
   fetch('/api/v1/items', {
     method: 'GET'
   }).then((response) => {
@@ -63,55 +58,26 @@ updateCount = () => {
   });
 };
 
+appendCount = (number) => {
+  const $spanNumber = $('.item-number');
+  $spanNumber.html(
+    `<span class='item-number'>${number}</span>`
+  );
+};
+
 appendItem = (name, cleanliness, id) => {
   const $itemCard = $('ul');
   $itemCard.prepend(
     `<li data-id='${id}' class='garage-list-item'>${name}</li>`
   );
-  updateCount()
+  getCount();
 };
 
-clearFields = () => {
- $('.name').val('');
- $('.reason').val('');
- $('.dropdown-form').val('');
-};
-
-$('ul').on('click', 'li', (e) => {
-  const id = e.target.dataset.id
-  grabItem(id);
-});
-
-
-const grabItem = (id) => {
-  fetch(`/api/v1/items/${id}`)
-  .then(response => response.json())
-  .then(items => {
-    items.map((item) => {
-      showItem(item.name, item.reason, item.cleanliness)
-    })
-  })
-}
-
-oneItem = (id) => {
-  fetch('/api/v1/items/${id}', {
-    method: 'GET'
-  }).then((response) => {
-    return response.json()
-  }).then((json) => {
-      showItem(json.name, json.reason, json.cleanliness)
-  }).catch((error) => {
-    error: 'cannot get that item'
-  });
-};
-
-showItem = (name, reason, cleanliness) => {
+appendOneItem = (name, reason, cleanliness) => {
   const $itemInfo = $('.item-info');
   $($itemInfo).empty().append(
     `<h3>${name}</h3>
     <h4>${reason}</h4>
-    <h4>${cleanliness}</h4>
-    <h5>Change Cleanliness</h5>
     <select class="dropdown-form form-field" placeholder="Cleanliness Level">
       <option class="cleanliness" value="" disabled selected>${cleanliness}</option>
       <option class='drop-item' value='sparkling'>Sparkling</option>
@@ -120,3 +86,28 @@ showItem = (name, reason, cleanliness) => {
     </select>`
   );
 };
+
+$('.submit').on('click', (e) => {
+  e.preventDefault();
+  let $name = $('.name').val();
+  let $reason = $('.reason').val();
+  let $cleanliness = $('.dropdown-form').val();
+  clearFields();
+  postItem($name, $reason, $cleanliness);
+});
+
+$('ul').on('click', 'li', (e) => {
+  const id = e.target.dataset.id;
+  grabItem(id);
+});
+
+clearFields = () => {
+ $('.name').val('');
+ $('.reason').val('');
+ $('.dropdown-form').val('');
+};
+
+$(".remote").click(function() {
+  $(this).toggleClass('on');
+  $(".door").toggleClass("shut");
+});
