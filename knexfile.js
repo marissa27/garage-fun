b5acdb7b068f18017d511a1f3126e4a5ca3161af
Marissa Reinke <reinke.marissa@gmail.com>
2017-06-01 09:40:00 -0600

    Setup knexfile and finish public folder setup.

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,30 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'pg',
+    connection: 'postgres://localhost/garage',
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/garagetest',
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: 'postgres://localhost/garage',
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  },
 
 };
